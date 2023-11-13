59e502dc80765916137428548b6bb6473cb0c509
0xMimir <mimirz@proton.me>
2023-11-13 15:33:48 +0100

    started adding jobs

--- src/job.rs
+++ src/job.rs
@@ -1,9 +1,11 @@
 use cron::Schedule;
+use async_trait::async_trait;
 
+#[async_trait]
 pub trait Job {
-    fn should_run(&self) -> bool{
-        true
-    }
+    // fn should_run(&self) -> bool{
+    //     true
+    // }
     fn schedule(&self) -> Schedule;
-    async fn job(&self);
+    // async fn job(&self);
 }
--- src/runner.rs
+++ src/runner.rs
@@ -1,11 +1,14 @@
-use std::sync::{
+use std::{sync::{
     atomic::{AtomicBool, Ordering},
-    Arc,
-};
+    Arc, RwLock,
+}, time::Duration};
 
-use tokio::task::JoinHandle;
+use tokio::{task::JoinHandle, time::sleep};
+
+use crate::job::Job;
 
 pub struct Cronus {
+    jobs: Arc<RwLock<Vec<Box<dyn Job>>>>,
     is_running: Arc<AtomicBool>,
     handle: JoinHandle<()>,
 }
@@ -15,9 +18,17 @@ impl Cronus {
         let handle = tokio::spawn(Self::spawn_worker());
 
         Self {
+            jobs: Arc::new(RwLock::default()),
             is_running: Arc::new(AtomicBool::new(false)),
             handle,
         }
+    }
+
+    pub fn add<T>(&self, job: T) where T: Job{
+        // let job = Box::new(job);
+        let f = RwLock::new(vec![1]);
+        let mut s = f.write().unwrap();
+        s.push(3);
     }
 
     pub fn is_running(&self) -> bool {
@@ -38,6 +49,11 @@ impl Cronus {
     }
 
     async fn spawn_worker() {
-        loop {}
+        loop {
+
+
+
+            sleep(Duration::from_millis(1)).await;
+        }
     }
 }
